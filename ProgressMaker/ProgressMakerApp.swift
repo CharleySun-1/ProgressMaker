a6b8f1a4a87abc18ef2e5bb7a5e6b7fc0627544d
CharleySun <Ysun@lcs.on.ca>
2021-02-16 14:29:15 -0500

    Initial Commit

---
+++ ProgressMaker/ProgressMakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProgressMakerApp.swift
+//  ProgressMaker
+//
+//  Created by Charley Sun on 2021-02-16.
+//
+
+import SwiftUI
+
+@main
+struct ProgressMakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
